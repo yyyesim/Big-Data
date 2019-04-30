51071412ad32043437e73cb1961911f0a037750b
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-01 02:21:24 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -24,8 +24,8 @@ my_list = [1,2,3,4,5,6,7,8,9,10]
 squared_list = map(lambda x:x**2,my_list)
 print squared_list
 
-#cntValue = pairs.reduceByKey(lambda accum, n: accum + n)
-#cntValue.pprint()
+cntValue = pairs.reduceByKey(lambda accum, n: accum + n)
+cntValue.pprint()
 
 #avgValue = pairs.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 #avgValue = pairs.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (y[0] + y[1], x[1] + y[1]))
