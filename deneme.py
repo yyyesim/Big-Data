200ef48ada4b3736fa34b630255abd1acb77ece4
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-01 01:49:13 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -10,7 +10,7 @@ lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
 
 # Split each line into words
 words = lines.flatMap(lambda line: line.split(" "))
-words.pprint()
+#words.pprint()
 
 # Count each word in each batch
 #pairs = words.map(lambda word: (word, 1))
