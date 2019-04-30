f67880f37e6796acc11253b8a8410ecdf8e376f1
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-04-30 19:15:31 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -15,10 +15,18 @@ words = lines.flatMap(lambda line: line.split(" "))
 #pairs = words.map(lambda word: (word, 1))
 pairs = words.map(lambda word: (word.split(",")[0], word))
 
+pprint('For (sensor,min)')
 #wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-wordCounts = pairs.reduceByKey(max)
+minValue = pairs.reduceByKey(min)
 # Print each batch
-wordCounts.pprint()
+minValue.pprint()
+
+print('For (sensor,max)')
+#wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+maxValue = pairs.reduceByKey(max)
+# Print each batch
+maxValue.pprint()
+
 
 ssc.start()             # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
