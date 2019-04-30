ffcd84e655fd0e8359dfa67b27cf31b5275148a1
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-01 00:06:04 +0300

    Update deneme1.py

--- deneme1.py
+++ deneme1.py
@@ -10,11 +10,11 @@ lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
 
 # Bazic reduceByKey example in python
 # creating PairRDD x with key value pairs
-x = sc.parallelize([("a", 1), ("b", 1), ("a", 1), ("a", 1),
+xx = sc.parallelize([("a", 1), ("b", 1), ("a", 1), ("a", 1),
                     ("b", 1), ("b", 1), ("b", 1), ("b", 1)], 3)
  
 # Applying reduceByKey operation on x
-y = x.reduceByKey(lambda accum, n: accum + n)
+y = xx.reduceByKey(lambda accum, n: accum + n)
 print(y.collect())
 # [('b', 5), ('a', 3)]
  
@@ -22,6 +22,6 @@ print(y.collect())
 def sumFunc(accum, n):
     return accum + n
  
-y = x.reduceByKey(sumFunc)
+y = xx.reduceByKey(sumFunc)
 print(y.collect())
 # [('b', 5), ('a', 3)]
