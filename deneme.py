3c43c256b13149740e068f19c8430b41f3b116cb
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-01 01:59:28 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -9,12 +9,12 @@ ssc = StreamingContext(sc, 20)
 lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
 
 # Split each line into words
-words = lines.flatMap(lambda line: line.split(" "))
-words.pprint()
+words2 = lines.flatMap(lambda line: line.split(" "))
+words2.pprint()
 
 # Count each word in each batch
 #pairs = words.map(lambda word: (word, 1))
-pairs = words.map(lambda word: (word.split(",")[0], word))
+pairs = words2.map(lambda word: (word.split(",")[0], word))
 
 
 
