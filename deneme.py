d5b8369ded940f25f0d42092322c6944c479fe96
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-04-30 20:16:02 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -15,20 +15,25 @@ words = lines.flatMap(lambda line: line.split(" "))
 #pairs = words.map(lambda word: (word, 1))
 pairs = words.map(lambda word: (word.split(",")[0], word))
 
-min = "For (sensor,min)"
-minString = sc.parallelize(List(min)).collect()
-minString.pprint()
+#min = "For (sensor,min)"
+#minString = sc.parallelize(List(min)).collect()
+#minString.pprint()
 
 #wordCounts = pairs.reduceByKey(lambda x, y: x + y)
 minValue = pairs.reduceByKey(min)
 # Print each batch
 minValue.pprint()
 
-print("For (sensor,max)")
+#print("For (sensor,max)")
 #wordCounts = pairs.reduceByKey(lambda x, y: x + y)
 maxValue = pairs.reduceByKey(max)
 # Print each batch
 maxValue.pprint()
+
+aTuple = (0,0) # As of Python3, you can't pass a literal sequence to a function.
+avg = pairs.aggregateByKey(aTuple, lambda a,b: (a[0] + b,    a[1] + 1), lambda a,b: (a[0] + b[0], a[1] + b[1]))
+avgValue = avg.mapValues(lambda v: v[0]/v[1]).collect()
+avgValue.pprint()
 
 ssc.start()             # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
