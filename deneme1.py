c0e3cc32ac3b906d93f460a2897f3802c9ba772c
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-01 00:04:36 +0300

    Update deneme1.py

--- deneme1.py
+++ deneme1.py
@@ -1,3 +1,13 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+# Create a local StreamingContext with two working threads and a batch interval of 2 seconds
+sc = SparkContext("local[2]", "Sensor")
+ssc = StreamingContext(sc, 20)
+
+# Create a DStream
+lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
+
 # Bazic reduceByKey example in python
 # creating PairRDD x with key value pairs
 x = sc.parallelize([("a", 1), ("b", 1), ("a", 1), ("a", 1),
