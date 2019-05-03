a93f59cb0fbf8f6429cc1910d4f00b54c661a2a2
yyyesim <50143208+yyyesim@users.noreply.github.com>
2019-05-03 17:04:04 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -3,7 +3,7 @@ from pyspark.streaming import StreamingContext
 
 # Create a local StreamingContext with two working threads and a batch interval of 2 seconds
 sc = SparkContext("local[2]", "Sensor")
-ssc = StreamingContext(sc, 20)
+ssc = StreamingContext(sc, 5)
 
 # Create a DStream
 lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
